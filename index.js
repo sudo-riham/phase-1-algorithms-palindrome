3a75a771b7ec189d78d62e18a0f9c72fab1f2897
Riham Zeidan <64324442+sudo-riham@users.noreply.github.com>
2023-01-06 14:48:13 +0200

    Assignment Done

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+   // find the length of a string
+    const len = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
